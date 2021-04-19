135770af6a251d0771efe4859a62f4fc208d7222
TimothyOwen <towen@qa.com>
2021-04-19 12:03:29 +0100

    Added table and buttons

--- src/main/java/com/to/nitor/components/InventoryWindow.java
+++ src/main/java/com/to/nitor/components/InventoryWindow.java
@@ -1,20 +1,53 @@
 package com.to.nitor.components;
 
+import java.util.List;
+
 import com.googlecode.lanterna.gui2.BasicWindow;
+import com.googlecode.lanterna.gui2.Button;
 import com.googlecode.lanterna.gui2.GridLayout;
 import com.googlecode.lanterna.gui2.Panel;
+import com.googlecode.lanterna.gui2.Window;
+import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
 import com.googlecode.lanterna.gui2.table.Table;
+import com.to.nitor.domain.Item;
+import com.to.nitor.utils.DBUtils;
 
 public class InventoryWindow extends BasicWindow {
-	public InventoryWindow() {
+	public InventoryWindow(final WindowBasedTextGUI textGUI) {
 		super("Inventory");
-		Panel containerPanel = new Panel(new GridLayout(4));
+		
+		DBUtils dbutils = new DBUtils("jdbc:mysql://localhost:3306/nitor","root","root");
+		
+		dbutils.getConnection();
+		
+		List<Item> items = dbutils.readAll();
+		
+		Panel containerPanel = new Panel(new GridLayout(1));
 		Table<String> table = new Table<String>("Crop", "Quantity", "Id");
-		table.getTableModel().addRow("Beetroot","2", "0540");
-		table.getTableModel().addRow("Potato","5", "9348");
-		table.getTableModel().addRow("Onion","1", "3985");
-		table.getTableModel().addRow("Mushroom","3", "1232");
-		table.getTableModel().addRow("Leek","10", "3124");
+		for(Item item: items) {
+			table.getTableModel().addRow(item.getName(),
+										String.valueOf(item.getQuantity()),
+										String.valueOf(item.getId()));
+		}
+		
+		dbutils.closeConnection();
+		
+		new Button("Back", new Runnable() {
+        	public void run() {
+        		Window activeWindow = textGUI.getActiveWindow();
+    			textGUI.removeWindow(activeWindow);
+    			textGUI.addWindowAndWait(new MenuWindow(textGUI));
+        	}
+        }).addTo(containerPanel);
+		
+		new Button("Add Item", new Runnable() {
+        	public void run() {
+        		Window activeWindow = textGUI.getActiveWindow();
+    			textGUI.removeWindow(activeWindow);
+    			textGUI.addWindowAndWait(new InventoryWindow(textGUI));
+        	}
+        }).addTo(containerPanel);
+		
 		containerPanel.addComponent(table);
 		setComponent(containerPanel);
 	}
