0db84a25e91f9e3c37382a5d4004371cb01719f7
TimothyOwen <towen@qa.com>
2021-04-19 12:03:07 +0100

    Added buttons

--- src/main/java/com/to/nitor/components/MenuWindow.java
+++ src/main/java/com/to/nitor/components/MenuWindow.java
@@ -5,9 +5,7 @@ import java.util.Arrays;
 import com.googlecode.lanterna.gui2.BasicWindow;
 import com.googlecode.lanterna.gui2.Button;
 import com.googlecode.lanterna.gui2.GridLayout;
-import com.googlecode.lanterna.gui2.Label;
 import com.googlecode.lanterna.gui2.Panel;
-import com.googlecode.lanterna.gui2.TextBox;
 import com.googlecode.lanterna.gui2.Window;
 import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
 
@@ -17,25 +15,24 @@ public class MenuWindow extends BasicWindow {
 		super("Nitor");
 		setHints(Arrays.asList(Window.Hint.CENTERED));
 		
-        Panel containerPanel = new Panel(new GridLayout(6));
+        Panel containerPanel = new Panel(new GridLayout(2));
         GridLayout gridLayout = (GridLayout)containerPanel.getLayoutManager();
         gridLayout.setHorizontalSpacing(3);
-        containerPanel.addComponent(new Label("Password"));
-        final TextBox passwordBox = new TextBox().addTo(containerPanel);
-        final Label buttonOutput = new Label("");
-        new Button("Log In", new Runnable() {
+        
+        new Button("Inventory", new Runnable() {
         	public void run() {
-        		CharSequence password = "root";
-        		if(passwordBox.getText().contentEquals(password)) {
-        			Window activeWindow = textGUI.getActiveWindow();
-        			textGUI.removeWindow(activeWindow);
-        			textGUI.addWindowAndWait(new InventoryWindow());
-        		}else {
-        			buttonOutput.setText(passwordBox.getText());
-        		}
+        		Window activeWindow = textGUI.getActiveWindow();
+    			textGUI.removeWindow(activeWindow);
+    			textGUI.addWindowAndWait(new InventoryWindow(textGUI));
         	}
         }).addTo(containerPanel);
-        containerPanel.addComponent(buttonOutput);
+        new Button("Exit", new Runnable() {
+        	public void run() {
+        		Window activeWindow = textGUI.getActiveWindow();
+    			textGUI.removeWindow(activeWindow);
+        	}
+        }).addTo(containerPanel);
+
         setComponent(containerPanel);
 	}
 }
