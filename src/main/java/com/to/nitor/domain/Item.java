5afb1116a5ecbfe2c9b1be2c906f81bc112614c1
TimothyOwen <towen@qa.com>
2021-04-19 15:19:18 +0100

    Overloaded constructor for add item method

--- src/main/java/com/to/nitor/domain/Item.java
+++ src/main/java/com/to/nitor/domain/Item.java
@@ -1,14 +1,29 @@
 package com.to.nitor.domain;
 
 public class Item {
+	
 	private String Name;
 	private Long Quantity;
 	private Long Id;
+	
+	public Item(String Name, Long Quantity) {
+		this.Name = Name;
+		this.Quantity = Quantity;
+		this.Id = null;
+	}
+	
 	public Item(String Name, Long Quantity, Long Id) {
 		this.Name = Name;
-		this.Id = Id;
 		this.Quantity = Quantity;
+		this.Id = Id;
 	}
+	
+	public Item(String Name, int Quantity, int Id) {
+		this.Name = Name;
+		this.Quantity = Long.valueOf(Quantity);
+		this.Id = Long.valueOf(Id);
+	}
+	
 	public String getName() {
 		return this.Name;
 	}
